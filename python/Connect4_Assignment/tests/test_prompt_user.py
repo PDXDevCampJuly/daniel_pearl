dc03c94274d46a9cb01fa3f6ad16d53b8776d173
Danielpearl <danielaaronpearl@gmail.com>
2015-08-20 15:03:43 -0700

    test written for prompt user

--- python/Connect4_Assignment/tests/test_prompt_user.py
+++ python/Connect4_Assignment/tests/test_prompt_user.py
@@ -3,6 +3,7 @@ __author__ = 'DanielPearl'
 import unittest
 from c4_view import C4View
 from unittest.mock import patch
+from io import StringIO
 
 class PromptTurnTest(unittest.TestCase):
     def setUp(self):
@@ -13,6 +14,12 @@ class PromptTurnTest(unittest.TestCase):
     def test_valid_single_input(self, return_value):
         """prompt is given a single input, returns single char string"""
         self.assertEqual("1",self.test_view.prompt_turn("Bob"))
+
+    @patch ('builtins.input', return_value='12')
+    def test_valid_multiple_input(self, return_value):
+        """prompt is given a multiple input, returns single char string"""
+        self.assertEqual("12",self.test_view.prompt_turn("Bob"))
+
 
 if __name__ == '__main__':
     unittest.main()
