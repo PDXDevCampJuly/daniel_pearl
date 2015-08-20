6495144af9e19bfa81c0074688ef8fbdcb059c23
Danielpearl <danielaaronpearl@gmail.com>
2015-08-20 11:15:02 -0700

    Fixing

--- python/Connect4_Assignment/Connect4/c4_view.py
+++ python/Connect4_Assignment/Connect4/c4_view.py
@@ -1,1 +1,47 @@
 __author__ = 'DanielPearl'
+
+class C4View:
+
+    """Connect 4 view"""
+
+    def __init__(self):
+        pass
+
+    def prompt_turn(self, name):
+        """
+        :param name: player name
+        :return: return column choice
+        """
+        pass
+
+    def show_instructions(self):
+        """
+         :print: instructions for game
+        """
+        pass
+
+    def win_statement(self):
+        """
+        :print: game winner
+        """
+        pass
+
+    def tie_statement(self):
+        """
+        :print: game tied
+        """
+        pass
+
+    def print_board(self, board):
+        """
+        :print: board
+        :param name: board list
+        """
+        pass
+
+    def prompt_name(self):
+        """
+        prompts user for name
+        :return: str name entered by user
+        """
+        pass
