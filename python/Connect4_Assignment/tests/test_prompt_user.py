3de8cda553210051e2a891426a9705ade73eaa24
Danielpearl <danielaaronpearl@gmail.com>
2015-08-20 14:48:19 -0700

    test written for prompt user

---
+++ python/Connect4_Assignment/tests/test_prompt_user.py
@@ -0,0 +1,18 @@
+__author__ = 'DanielPearl'
+
+import unittest
+from c4_view import C4View
+from unittest.mock import patch
+
+class PromptTurnTest(unittest.TestCase):
+    def setUp(self):
+        self.test_view = C4View()
+        print(self.shortDescription())
+
+    @patch ('builtins.input', return_value='1')
+    def test_valid_single_input(self, return_value):
+        """prompt is given a single input, returns single char string"""
+        self.assertEqual("1",self.test_view.prompt_turn("Bob"))
+
+if __name__ == '__main__':
+    unittest.main()
