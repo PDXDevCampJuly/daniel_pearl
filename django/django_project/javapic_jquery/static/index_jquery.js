c369c2c661979154bf3c2b23f79a5eee4e0110a2
Danielpearl <danielaaronpearl@gmail.com>
2015-09-14 10:27:35 -0700

    jquery image calls updated

--- django/django_project/javapic_jquery/static/index_jquery.js
+++ django/django_project/javapic_jquery/static/index_jquery.js
@@ -8,9 +8,9 @@ function update_jumbotron(){
     //Creates a slideshow of photos
 
     if (count < 10){
-        $(".jumbotron").css("background-image", "url(\"images/pdxcg_0" + count + ".jpg\")");
+        $(".jumbotron").css("background-image", "url(\"../static/images/pdxcg_0" + count + ".jpg\")");
     } else {
-        $(".jumbotron").css("background-image", "url(\"images/pdxcg_" + count + ".jpg\")");
+        $(".jumbotron").css("background-image", "url(\"../static/images/pdxcg_" + count + ".jpg\")");
     }
     if (count == 60){
         count = 1; //Resets timer
